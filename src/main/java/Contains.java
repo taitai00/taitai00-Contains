35085f76c5101a15fd3406141ffccba62401cef7
Khup Taithul <aatuang@gmail.com>
2023-02-28 01:01:50 +0000

    fixed the method

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -11,6 +11,9 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
+        for(Integer i: arr){
+            if (i == target) return true;
+        }
         return false;
     }
 }
